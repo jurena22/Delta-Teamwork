319842fb5650c54473d7e3e24b89e7d0fc9f696d
Nahiti <toth.dzsesszi@gmail.com>
2022-11-24 21:48:43 +0100

    carousel

--- src/app/core/components/mainpage/mainpage.component.ts
+++ src/app/core/components/mainpage/mainpage.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainpageComponent implements OnInit {
   wordText1: string[] = ['Új kihívásra vágysz?', 'Kipróbálnál valami újat?', 'Érdekel az IT világa?', 'Munkahelyváltás előtt állsz?', 'PROGmasters?'];
-
+  images = [
+    {
+      title: 'testz',
+      secodTitle: '1',
+      url:"assets/images/new-job.jpg",
+    },
+    {
+      title: 'teszt2',
+      secodTitle: '2',
+      url:"assets/images/languages.png",
+    },
+    {
+      title: 'teszt3',
+      secodTitle: '3',
+      url:"assets/images/softskills.jpg",
+    },
+    {
+      title: 'teszt4',
+      secodTitle: '4',
+      url:"assets/images/partners.jpg",
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
